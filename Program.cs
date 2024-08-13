c7b778e6bd2c7c475a8c6fb0a1697c334f881d17
Laryssa Lorene <laryssaloaf@gmail.com>
2024-08-13 15:24:20 -0300

    desafio concluído!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Iphone i1 = new Iphone("558420203128", "a23", "IMEI.info: 356303489648129",28);
+i1.InstalarAplicativo("Pou");
+i1.Ligar();
+i1.ReceberLigacao();
+
+Nokia n1 = new Nokia("558420203128", "n23", "IMEI.info: 356303489648129",25);
+n1.InstalarAplicativo("Pet play");
+n1.Ligar();
+n1.ReceberLigacao();
